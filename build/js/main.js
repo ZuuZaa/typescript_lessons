d3af7cc5f7541953dd9c2af68871fad0df072f74
ZuuZaa <zumbaghirova@gmail.com>
2023-05-16 17:19:35 +0400

    objects

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,32 @@
 "use strict";
 const user = 'zumrud';
 console.log(user);
+const a = 12;
+const b = '6';
+const c = 2;
+let d;
+console.log(a / c);
+console.log(a * c);
+d = true;
+d = "hello";
+d = 42;
+const sum = (a, b) => {
+    return a + b;
+};
+const arr1 = [1, 2, 3];
+let arr2 = ['hello', 2, false];
+let arr3 = ['hey', 19, true];
+// arr3 = arr2 
+arr2 = arr3;
+const obj1 = {
+    name: 'zumrud',
+    age: 40,
+    active: true
+};
+const user1 = {
+    name: 'zumrud',
+    age: 40
+};
+const greeting = (user) => {
+    return user.active || false;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,48 @@
 const user = 'zumrud'
 console.log(user)
+
+
+const a: number = 12
+const b: string = '6'
+const c: number = 2
+let d: any;
+
+console.log(a/c)
+console.log(a*c)
+d= true;
+d= "hello"
+d=42
+
+const sum = (a:number,b:number ) => {
+    return a+b;
+} 
+
+
+const arr1  = [1,2, 3]
+let arr2 = ['hello', 2,false]
+let arr3 : [string, number, boolean] = ['hey', 19, true]
+// arr3 = arr2 
+arr2 = arr3
+
+const obj1 = {
+    name: 'zumrud',
+    age: 40,
+    active: true
+}
+
+interface User {
+    name: string,
+    age: number,
+    active?: boolean,
+    skills?: string[]
+
+}
+
+const user1: User = {
+    name: 'zumrud',
+    age: 40
+} 
+
+const greeting = (user: User) => {
+   return user.active || false;
+}
