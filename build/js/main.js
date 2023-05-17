d6482fc9a4a51647611faa5b6007cf4312732cc3
ZuuZaa <zumbaghirova@gmail.com>
2023-05-17 12:03:24 +0400

    functions

--- build/js/main.js
+++ build/js/main.js
@@ -15,6 +15,7 @@ const sum = (a, b) => {
 };
 const arr1 = [1, 2, 3];
 let arr2 = ['hello', 2, false];
+let arr4 = ['hey', 'hello', false, 87];
 let arr3 = ['hey', 19, true];
 // arr3 = arr2 
 arr2 = arr3;
@@ -30,3 +31,47 @@ const user1 = {
 const greeting = (user) => {
     return user.active || false;
 };
+const john = {
+    id: 1,
+    name: 'John',
+    albums: ["hello", 37]
+};
+// ------- functions ------
+const logMessage = (message) => {
+    console.log(`your message is: ${message}`);
+};
+const add = (a, b) => {
+    return a + b;
+};
+logMessage("hello");
+logMessage(add(2, 5));
+// interface MathFunc {
+//     (a:number, b:number) : number
+// } 
+const substract = (c, d) => {
+    return c - d;
+};
+logMessage(substract(32, 5));
+// ----- optional parameters ----------
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined')
+        return a + b + c;
+    return a + b;
+};
+// ----- default parameters ----------
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+logMessage(addAll(2, 3, 4));
+logMessage(addAll(2, 3));
+logMessage(sumAll(2, 3, 4));
+logMessage(sumAll(2, 3));
+// ------ rest parameters --------
+const total = (...num) => {
+    return num.reduce((prev, curr) => prev + curr);
+};
+logMessage(total(2, 4, 5));
+const addToNum = (a, ...num) => {
+    return a + num.reduce((prev, curr) => prev + curr);
+};
+logMessage(addToNum(2, 4));
--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,22 @@ const b: string = '6'
 const c: number = 2
 let d: any;
 
-console.log(a/c)
-console.log(a*c)
-d= true;
-d= "hello"
-d=42
+console.log(a / c)
+console.log(a * c)
+d = true;
+d = "hello"
+d = 42
 
-const sum = (a:number,b:number ) => {
-    return a+b;
-} 
+const sum = (a: number, b: number) => {
+    return a + b;
+}
 
 
-const arr1  = [1,2, 3]
-let arr2 = ['hello', 2,false]
-let arr3 : [string, number, boolean] = ['hey', 19, true]
+const arr1 = [1, 2, 3]
+let arr2 = ['hello', 2, false]
+let arr4: (string | number | boolean)[] = ['hey', 'hello', false, 87]
+let arr3: [string, number, boolean] = ['hey', 19, true]
+
 // arr3 = arr2 
 arr2 = arr3
 
@@ -41,8 +43,86 @@ interface User {
 const user1: User = {
     name: 'zumrud',
     age: 40
-} 
+}
 
 const greeting = (user: User) => {
-   return user.active || false;
+    return user.active || false;
 }
+
+
+// ----- type -------
+type UserId = string | number
+
+type Albums = (string | number)[]
+
+type Name = 'John' | 'Amy' | 'David'
+
+type Gitarist = {
+    id: UserId,
+    name: Name,
+    albums: Albums
+}
+
+const john: Gitarist = {
+    id: 1,
+    name: 'John',
+    albums: ["hello", 37]
+}
+
+// ------- functions ------
+
+const logMessage = (message: any): void => {
+    console.log(`your message is: ${message}`)
+}
+
+const add = (a: number, b: number): number => {
+    return a + b;
+}
+
+logMessage("hello")
+logMessage(add(2, 5))
+
+type MathFunc = (a: number, b: number) => number
+
+// interface MathFunc {
+//     (a:number, b:number) : number
+// } 
+
+const substract: MathFunc = (c, d) => {
+    return c - d;
+}
+
+logMessage(substract(32, 5))
+
+
+// ----- optional parameters ----------
+
+const addAll = (a: number, b: number, c?: number): number => {
+    if (typeof c !== 'undefined') return a + b + c
+    return a + b
+}
+// ----- default parameters ----------
+
+const sumAll = (a: number, b: number, c: number = 2): number => {
+    return a + b + c
+}
+
+logMessage(addAll(2, 3, 4))
+logMessage(addAll(2, 3))
+logMessage(sumAll(2, 3, 4))
+logMessage(sumAll(2, 3))
+
+
+// ------ rest parameters --------
+
+const total = (...num: number[]): number => {
+    return num.reduce((prev, curr) => prev + curr)
+}
+
+logMessage(total(2, 4, 5))
+
+const addToNum = (a: number, ...num: number[]): number => {
+    return a + num.reduce((prev, curr) => prev + curr)
+}
+
+logMessage(addToNum(2, 4))
