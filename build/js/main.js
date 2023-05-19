5c13cd63607a9c75e23a8a0705737c9ba1262948
ZuuZaa <zumbaghirova@gmail.com>
2023-05-19 12:33:02 +0400

    dom

--- build/js/main.js
+++ build/js/main.js
@@ -36,7 +36,7 @@ const john = {
     name: 'John',
     albums: ["hello", 37]
 };
-// ------- functions ------
+// ------- FUNCTIONS ------
 const logMessage = (message) => {
     console.log(`your message is: ${message}`);
 };
@@ -75,3 +75,66 @@ const addToNum = (a, ...num) => {
     return a + num.reduce((prev, curr) => prev + curr);
 };
 logMessage(addToNum(2, 4));
+// ------ never -------
+// const newFunc = (num: number): never => {
+//     while (true) {
+//         num++
+//     }
+//     throw console.error("loop error");
+// }
+// ------- custom type guard ------
+const isNumber = (value) => typeof value === "number" ? true : false;
+const numberOrString = (value) => {
+    if (isNumber(value))
+        return 'number';
+    if (typeof value === "string")
+        return 'string';
+    return 'error';
+};
+// convert to more or less specific type
+const one = 'hello';
+const two = one; // less specific 
+const three = one; // more specific
+const four = 'world'; // this type coding cannot be used in react
+const five = 'love';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+const myStringOfNumbers = addOrConcat(2, 3, 'concat'); // because it can also return number 
+const myNumber = addOrConcat(2, 3, 'concat'); // be carefully! TS sees no problem, but there is a problem
+// 10 as string  // TS doesn't like this but
+10; // it is possible
+// ---------- DOM -----------
+const image = document.querySelector('img'); // its an HTML element or null 
+const image1 = document.querySelector('#img'); // its just an element or null 
+const image2 = document.getElementById('#img'); // now its an HTML element or null 
+// image.src  doesn't like it, because it can be null, but
+const image3 = document.querySelector('img'); // not null, if you know that html element exist
+const image4 = document.querySelector('img'); // just an element 
+// image4.src // there is no src attribute of html element 
+const image5 = document.querySelector('img'); // it is image element 
+image5.src; // there is no problem now
+const image6 = document.querySelector('img'); // cannot be used in react
+// original js code 
+// const yearSpan = document.querySelector('.year') 
+// const currentYear = new Date().getFullYear() 
+// yearSpan.setAttribute('date', currentYear)
+// yearSpan.textContent = currentYear
+// 1st solution
+// let yearSpan: HTMLElement | null 
+// yearSpan = document.querySelector('.year') 
+// let currentYear : string
+// currentYear = new Date().getFullYear().toString()
+// if (yearSpan) {
+//     yearSpan.setAttribute('date', currentYear)
+//     yearSpan.textContent = currentYear
+// }
+// 2nd solution
+const yearSpan = document.querySelector('.year');
+const currentYear = new Date().getFullYear().toString();
+console.log(yearSpan, currentYear);
+yearSpan.setAttribute('date', currentYear);
+yearSpan.textContent = currentYear;
+// --------- CLASSES --------------
--- src/main.ts
+++ src/main.ts
@@ -69,7 +69,7 @@ const john: Gitarist = {
     albums: ["hello", 37]
 }
 
-// ------- functions ------
+// ------- FUNCTIONS ------
 
 const logMessage = (message: any): void => {
     console.log(`your message is: ${message}`)
@@ -126,3 +126,95 @@ const addToNum = (a: number, ...num: number[]): number => {
 }
 
 logMessage(addToNum(2, 4))
+
+
+// ------ never -------
+
+// const newFunc = (num: number): never => {
+
+
+//     while (true) {
+//         num++
+//     }
+//     throw console.error("loop error");
+
+// }
+
+// ------- custom type guard ------
+const isNumber = (value: any): boolean => typeof value === "number" ? true : false
+
+const numberOrString = (value: number | string): string => {
+    if (isNumber(value)) return 'number';
+    if (typeof value === "string") return 'string';
+    return 'error'
+}
+
+
+// ------- type assertions ------
+
+type One = string
+type Two = number | string
+type Three = 'hello'
+
+// convert to more or less specific type
+
+const one: One = 'hello'
+const two = one as Two // less specific 
+const three = one as Three // more specific
+
+const four = <One>'world' // this type coding cannot be used in react
+const five = <string | number>'love'
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): string | number => {
+    if (c === 'add') return a + b
+    return '' + a + b
+}
+
+const myStringOfNumbers: string = addOrConcat(2, 3, 'concat') as string // because it can also return number 
+const myNumber: number = addOrConcat(2, 3, 'concat') as number // be carefully! TS sees no problem, but there is a problem
+
+// 10 as string  // TS doesn't like this but
+(10 as unknown) as string // it is possible
+
+
+// ---------- DOM -----------
+
+const image = document.querySelector('img') // its an HTML element or null 
+const image1 = document.querySelector('#img') // its just an element or null 
+const image2 = document.getElementById('#img') // now its an HTML element or null 
+
+// image.src  doesn't like it, because it can be null, but
+const image3 = document.querySelector('img')!  // not null, if you know that html element exist
+const image4 = document.querySelector('img') as HTMLElement // just an element 
+// image4.src // there is no src attribute of html element 
+const image5 = document.querySelector('img') as HTMLImageElement // it is image element 
+image5.src // there is no problem now
+
+const image6 = <HTMLImageElement>document.querySelector('img') // cannot be used in react
+
+// original js code 
+// const yearSpan = document.querySelector('.year') 
+// const currentYear = new Date().getFullYear() 
+// yearSpan.setAttribute('date', currentYear)
+// yearSpan.textContent = currentYear
+
+// 1st solution
+// let yearSpan: HTMLElement | null 
+// yearSpan = document.querySelector('.year') 
+// let currentYear : string
+// currentYear = new Date().getFullYear().toString()
+// if (yearSpan) {
+//     yearSpan.setAttribute('date', currentYear)
+//     yearSpan.textContent = currentYear
+// }
+
+// 2nd solution
+const yearSpan = document.querySelector('.year') as HTMLSpanElement
+const currentYear: string = new Date().getFullYear().toString()
+console.log(yearSpan, currentYear)
+yearSpan.setAttribute('date', currentYear)
+yearSpan.textContent = currentYear
+
+
+// --------- CLASSES --------------
+
